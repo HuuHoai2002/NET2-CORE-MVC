cf303afc9e3b608661e3a99edd23eb695ca1e401
Huuhoai2002 <huuhoai0511@gmail.com>
2022-08-25 10:04:16 +0700

    first commit: init project

---
+++ NET2-Huuhoai2002/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NET2_Huuhoai2002.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Lập Trình .NET 2 + BTL
+
+## Nghiêm Hữu Hoài
+
+## Msv: 2021050273
+
+## Email: huuhoai0511@gmail.com
